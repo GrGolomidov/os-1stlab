179d7bb8cd106b3bb33291cb9c69171b0a703e1f
alinanasonova <47684560+alinanasonova@users.noreply.github.com>
2020-09-15 12:58:04 -0400

    read, 1, 2.1, 3.1

--- l1.sh
+++ l1.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 check=$1
+a1=$2
+a2=$3
+a3=$4
+a4=$5
 
-if [[ $check = '-h' ]]; then
+#if [[ $check = '--help'  || $check = '-h' ]]; then
+case "$check" in
+"-h"|"--help")
     printf "Авторы:\n\tАлександр Шихалев\n\tАлина Насонова\n\tГригорий Голомидов\n
 Все доступные аргументы:\n\t-i - Вывод всех сетевых интерфейсов
 \t-o - Включение/отсключение заданных интерфейсов
@@ -11,5 +17,17 @@ if [[ $check = '-h' ]]; then
 \t-m - Построение карты сети\n\t-p - Отображение сетевой статистики
 Кратное описание проекта:
 \tПроект позволяет управлять сетевыми настройками системы с помощью аргументов.
-Примеры запуска:\n\tl1.sh -h\n\tl1.sh -o\n"
-fi
+Примеры запуска:\n\tl1.sh -h\n\tl1.sh -o\n";;
+"-k")
+    kill -9 $a1;;
+"-i")
+    printf "Имя сетевого инт.\tMAC адрес\t\tIP адрес\t\tСкорость соединения\n"
+    for i in $(ls /sys/class/net/)
+    do
+	printf "$i\t\t$(ifconfig $i | grep "ether" | awk '{print $2}')\t\t$(ifconfig $i | grep "inet " | awk '{print $2}')\t\t$(ifconfig $i | grep "RX packets" | awk '{print $6, $7}')\n"
+    done
+#"-o")
+#    case "$a1" in
+#    "up")
+	
+esac
