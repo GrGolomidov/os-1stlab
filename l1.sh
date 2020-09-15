2b35c346ab5303963ef7e930edadf4c0c52dd770
alinanasonova <47684560+alinanasonova@users.noreply.github.com>
2020-09-15 15:43:47 -0400

    2.2

--- l1.sh
+++ l1.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
 check=$1
-a1=$2
-a2=$3
-a3=$4
-a4=$5
 
-#if [[ $check = '--help'  || $check = '-h' ]]; then
 case "$check" in
 "-h"|"--help")
     printf "Авторы:\n\tАлександр Шихалев\n\tАлина Насонова\n\tГригорий Голомидов\n
@@ -17,17 +12,20 @@ case "$check" in
 \t-m - Построение карты сети\n\t-p - Отображение сетевой статистики
 Кратное описание проекта:
 \tПроект позволяет управлять сетевыми настройками системы с помощью аргументов.
-Примеры запуска:\n\tl1.sh -h\n\tl1.sh -o\n";;
+Примеры запуска:\n\tl1.sh -o down <int_name1> up <int_name2>\n\tl1.sh -k <port>\n";;
 "-k")
-    kill -9 $a1;;
+    kill -9 $2;;
 "-i")
     printf "Имя сетевого инт.\tMAC адрес\t\tIP адрес\t\tСкорость соединения\n"
     for i in $(ls /sys/class/net/)
     do
 	printf "$i\t\t$(ifconfig $i | grep "ether" | awk '{print $2}')\t\t$(ifconfig $i | grep "inet " | awk '{print $2}')\t\t$(ifconfig $i | grep "RX packets" | awk '{print $6, $7}')\n"
-    done
-#"-o")
-#    case "$a1" in
-#    "up")
-	
+    done;;
+"-o")
+    q=$#
+    for (( i=2, j=3; i <= q; i+=2, j+=2 ))
+    do
+	sudo ip link set ${!j} ${!i}
+	#sudo "if${!i}" ${!j}
+    done;;
 esac
